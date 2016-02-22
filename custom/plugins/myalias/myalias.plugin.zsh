82e3b0d9da9ad0b5538fef4b23572ea3dff9dcbf
Gullit Miranda <gullitmiranda@gmail.com>
2016-02-22 14:28:15 -0300

    Updating myalias

--- custom/plugins/myalias/myalias.plugin.zsh
+++ custom/plugins/myalias/myalias.plugin.zsh
@@ -1,4 +1,15 @@
+function exportf () {
+  export $(echo $1)="`whence -f $1 | sed -e "s/$1 //" `"
+}
+
 alias  gfa='git fetch --all'
 alias gfap='git fetch --all --prune'
 alias  gmn='git merge --no-ff'
 alias grbm='git rebase master $(current_branch)'
+alias gpc='git push origin $(current_branch)'
+alias glu='git pull upstream $(current_branch)'
+alias gtm='git tag -a -m  -f'
+
+exportf current_branch
+exportf git_prompt_short_sha
+alias commit="$(git_prompt_short_sha)"
